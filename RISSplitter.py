bec9751a5a7348b11cd457b91ed0bb37fc14acbf
William Tichenor <williamtichenor@tamu.edu>
2025-02-03 18:00:10 -0600

    finalized first iter

--- RISSplitter.py
+++ RISSplitter.py
@@ -3,10 +3,25 @@ import rispy
 
 
 fpath = sys.argv[1]
-print("Segments: "+sys.argv[2])
+segs = int(sys.argv[2])
+print("Segments: "+str(segs))
 with open(fpath, 'r', encoding="utf-8-sig") as dataFile:
     data = rispy.load(dataFile)
+    outputContent = [[] for _ in range(segs)]
+    totalNum = len(data)
+    print("Total Entries: "+str(totalNum))
+    numPer = int(totalNum/segs)
     i = 0
     for entry in data:
-        print(entry['title'])
+        fileNo = int(i/numPer)
+        if fileNo >= segs:
+            fileNo = segs-1
+        outputContent[fileNo].append(entry)
+        i += 1
+    i = 0
+    for content in outputContent:
+        print(str(len(content))+" Entries")
+        outpath = 'output/output-'+str(i)+'.ris'
+        with open(outpath, 'w', encoding="utf-8-sig") as dataFile:
+            rispy.dump(content, dataFile)
         i += 1
